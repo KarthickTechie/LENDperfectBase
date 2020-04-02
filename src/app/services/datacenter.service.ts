ed002c0fb910d12c064f127e238f3e6614e43115
Kamal1214 <47447481+Kamal1214@users.noreply.github.com>
2020-04-02 20:29:13 +0530

    Added Method getLoanAmountAppliedGreaterThan50000

--- src/app/services/datacenter.service.ts
+++ src/app/services/datacenter.service.ts
@@ -6,11 +6,15 @@ export class DatacenterService {
 
   applications:Applications[] = [
     new Applications(1,'John','50000','notsubmitted'),
-    new Applications(2,'Mike','50000','notsubmitted'),
-    new Applications(3,'Steve','50000','notsubmitted'),
-    new Applications(4,'Bob','50000','notsubmitted'),
-    new Applications(5,'Tom','50000','notsubmitted'),
-    new Applications(6,'Jerry','50000','notsubmitted'),
+    new Applications(2,'Mike','150000','notsubmitted'),
+    new Applications(3,'Steve','75000','notsubmitted'),
+    new Applications(4,'Bob','250000','notsubmitted'),
+    new Applications(5,'Tom','350000','notsubmitted'),
+    new Applications(6,'Jerry','450000','notsubmitted'),
+    new Applications(3,'Adam','90000','notsubmitted'),
+    new Applications(4,'James','125000','notsubmitted'),
+    new Applications(5,'David','50000','notsubmitted'),
+    new Applications(6,'Alen','50000','notsubmitted'),
   ]
 
   constructor() { }
@@ -27,6 +31,11 @@ export class DatacenterService {
   getSubmittedApplicationCount():number{
       
     return this.applications.filter(item=>item.status=='submitted').length;
+  }
+  
+  getLoanAmountAppliedGreaterThan50000():Applications[]{
+   
+    return this.applications.filter(item=>item.loanamount>50000)
   }
 
 }
