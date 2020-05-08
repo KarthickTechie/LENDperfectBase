8ae66e3a98aaab5e961c016a12e9c354f4260f3c
KarthickTechie <reachkarthickprogramer@gmail.com>
2020-05-09 01:08:54 +0530

    added QR OCR Document Scanner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,11 @@ import { ReactiveFormsModule } from "@angular/forms";
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+import { OCR } from '@ionic-native/ocr/ngx';
+import { Camera } from '@ionic-native/camera/ngx';
+import { FilePath } from '@ionic-native/file-path/ngx';
+
 
 import { ColorPickerModule } from "ngx-color-picker";
 import { IonicStorageModule } from '@ionic/storage';
@@ -16,10 +21,23 @@ import { SQLite } from '@ionic-native/sqlite/ngx';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, ColorPickerModule, ReactiveFormsModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot()],
+  imports: [
+    BrowserModule,
+    ColorPickerModule,
+    ReactiveFormsModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    IonicStorageModule.forRoot()
+  ],
   providers: [
     StatusBar,
-    SplashScreen, SQLite, SqliteProvider,
+    SplashScreen,
+    SQLite,
+    SqliteProvider,
+    BarcodeScanner,
+    OCR,
+    Camera,
+    FilePath,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
