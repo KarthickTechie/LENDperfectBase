4a3895bf0f5372c21d769e55614eddec9b6d726d
praveen21692 <v.praveenraj21692@gmail.com>
2020-05-20 00:08:57 +0530

    Error Handling Service Added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,7 +19,11 @@ const routes: Routes = [
   {
     path: 'setting',
     loadChildren: () => import('./setting/setting.module').then(m => m.SettingPageModule)
+  },  {
+    path: 'document-upload',
+    loadChildren: () => import('./document-upload/document-upload.module').then( m => m.DocumentUploadPageModule)
   },
+
   
 ];
 
